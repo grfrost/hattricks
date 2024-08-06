2c0c8d552676c5d069d1d32f242784fbba4083a3
grfrost <frost.gary@gmail.com>
2024-08-06 17:52:18 +0100

    colors

--- chess/src/main/java/chess/Main.java
+++ chess/src/main/java/chess/Main.java
@@ -12,68 +12,42 @@ import java.lang.runtime.CodeReflection;
 
 public class Main {
 
-    static public final int neighbourDxDy = 0b00_00__01_00__10_00__00_01__10_01__00_10__01_10__01_01;
+    static public final int compassDxDy = 0b00_00__01_00__10_00__00_01__10_01__00_10__01_10__01_01;
     //                                        -1 -1,  0 -1,  1 -1, -1, 0,  1, 0, -1  1,  0, 1,  1, 1
     //                                          nw     n      ne     w      e      sw     s      se
-    static public final byte EMPTY_SQUARE = (byte) 0x00;
-    static public final byte PAWN_VALUE = (byte) 0x01;
-    static public final byte KNIGHT_VALUE = (byte) 0x02;
-    static public final byte BISHOP_VALUE = (byte) 0x03;
-    static public final byte ROOK_VALUE = (byte) 0x04;
-    static public final byte QUEEN_VALUE = (byte) 0x05;
-    static public final byte KING_VALUE = (byte) 0x06;
-    static public final byte PIECE_MASK = (byte) 0b0000_0111;
-    static public final byte HOME_BIT = (byte) 0b0000_1000;
-    static public final byte BLACK_BIT = (byte) 0b0001_0000;
-    static public final byte WHITE_BIT = (byte) 0b0010_0000;
-    static public final byte COLOR_MASK = WHITE_BIT | BLACK_BIT;
+    public static final byte DIAGS = (byte) 0b1010_0101;
+    public static final byte COLROWS = (byte) 0b0101_1010;
+    public static final byte ALL_POINTS = (byte) 0b1111_1111;
+    static public final byte EMPTY_SQUARE = (byte) 0b0000_0000;
+    static public final byte PAWN_VALUE =   (byte) 0b0000_0001;
+    static public final byte KNIGHT_VALUE = (byte) 0b0000_0010;
+    static public final byte BISHOP_VALUE = (byte) 0b0000_0011;
+    static public final byte ROOK_VALUE =   (byte) 0b0000_0100;
+    static public final byte QUEEN_VALUE =  (byte) 0b0000_0101;
+    static public final byte KING_VALUE =   (byte) 0b0000_0110;
+    static public final byte WHITE_BIT =    (byte) 0b0000_1000;
+    static public final byte PIECE_MASK =   (byte) 0b0000_0111;
+
     static public final byte OFF_BOARD_SQUARE = (byte) 0b1111_1111;
-    static public final short MOVE_BIT = (short) 0b0000_0001_0000_0000;
-    static public final short CAPTURE_BIT = (short) 0b0000_0010_0000_0000;
-
-    //static public final long DIAG_BITS =          0b10011001_010011010_001011100_000111;
-
-
     public static class Compute {
         @CodeReflection
         public static boolean isWhite(byte squareBits) {
-            return (squareBits & COLOR_MASK) == WHITE_BIT;
+            return (squareBits & WHITE_BIT) == WHITE_BIT;
         }
 
         @CodeReflection
         public static boolean isBlack(byte squareBits) {
-            return (squareBits & COLOR_MASK) == BLACK_BIT;
+            return (squareBits & WHITE_BIT) != WHITE_BIT;
         }
 
         @CodeReflection
         public static boolean isOpponent(byte mySquareBits, byte opponentSquareBits) {
-            return ((mySquareBits | opponentSquareBits) & COLOR_MASK) == COLOR_MASK;
-        }
-
-        @CodeReflection
-        public static boolean isMyPiece(byte mySquareBits, byte opponentSquareBits) {
-            return (mySquareBits & opponentSquareBits & COLOR_MASK) == mySquareBits;
+            return ((mySquareBits ^ opponentSquareBits) & WHITE_BIT) == WHITE_BIT;
         }
 
         @CodeReflection
         public static boolean isEmpty(byte squareBits) {
             return (squareBits & PIECE_MASK) == 0;
-        }
-
-        @CodeReflection
-        public static boolean isHome(byte squareBits) {
-            return (squareBits & HOME_BIT) == HOME_BIT;
-        }
-
-        @CodeReflection
-        static boolean isEmpty(ChessData.Board board, int x, int y) {
-            return isEmpty(board.squareBits(x + y * 8L));
-        }
-
-        @CodeReflection
-        static boolean isOpponent(ChessData.Board board, int x, int y, byte myBits) {
-            byte opponentSquareBits = board.squareBits(x + y * 8L);
-            return isOpponent(myBits, opponentSquareBits);
         }
 
         @CodeReflection
@@ -105,16 +79,16 @@ public class Main {
         static boolean isKing(int squareBits) {
             return (squareBits & PIECE_MASK) == KING_VALUE;
         }
+        @CodeReflection
+        static boolean isPiece(int squareBits) {
+            return (squareBits & PIECE_MASK) != 0;
+        }
 
         @CodeReflection
         static boolean isQueen(int squareBits) {
             return (squareBits & PIECE_MASK) == QUEEN_VALUE;
         }
 
-        @CodeReflection
-        static boolean isEmpty(int squareBits) {
-            return (squareBits & PIECE_MASK) == EMPTY_SQUARE;
-        }
 
         @CodeReflection
         static boolean isOffBoard(int squareBits) {
@@ -179,17 +153,19 @@ public class Main {
             }
 
             default void validMoves(byte squareBits, int fromx, int fromy) {
-                PIECE piece = PIECE.of(squareBits);
-                if (piece.count > 0) {
-                    int compassBits = piece.compassBits & 0xff;
+              //  PIECE piece = PIECE.of(squareBits);
+                int pieceValue = squareBits&PIECE_MASK;
+                if (pieceValue >= KNIGHT_VALUE) {
+                    int compassBits = (pieceValue==BISHOP_VALUE?DIAGS:((pieceValue==ROOK_VALUE)?COLROWS:ALL_POINTS));
                     int blockedBits = compassBits ^ 0xff;
-                    for (int v = 1; v <= piece.count; v++) {
+                    int count = pieceValue<KING_VALUE?7:1;
+                    for (int v = 1; v <= count; v++) {
                         int compassBit = 0b1000_0000;
 
                         //                     nw    n   ne   w     e   sw    s   se
                         int neighbourMask = 0b1111_0000_0000_0000_0000_0000_0000_0000;
                         for (int neighbourDxDyIdx = 7; neighbourDxDyIdx > 0; neighbourDxDyIdx--) {
-                            int nb = (neighbourMask & neighbourDxDy);
+                            int nb = (neighbourMask & compassDxDy);
                             int nbshifted = nb >>> (neighbourDxDyIdx * 4);
                             int x = ((nbshifted >>> 2) & 0b11) - 1;
                             int y = (nbshifted & 0b11) - 1;
@@ -230,30 +206,30 @@ public class Main {
                 }
 
                 for (int x = 0; x < 8; x++) {
-                    squareBits((long)(1*8+x), (byte) (PAWN_VALUE | BLACK_BIT | HOME_BIT));
-                    squareBits((long)(6*8+x), (byte) (PAWN_VALUE | WHITE_BIT | HOME_BIT));
+                    squareBits((long)(1*8+x), (byte) (PAWN_VALUE ));
+                    squareBits((long)(6*8+x), (byte) (PAWN_VALUE | WHITE_BIT ));
                 }
 
-                squareBits((long)(0*8+0), (byte) (ROOK_VALUE | BLACK_BIT | HOME_BIT));
-                squareBits((long)(0*8+7), (byte) (ROOK_VALUE | BLACK_BIT | HOME_BIT));
-                squareBits((long)(7*8+0), (byte) (ROOK_VALUE | WHITE_BIT | HOME_BIT));
-                squareBits((long)(7*8+7), (byte) (ROOK_VALUE | WHITE_BIT | HOME_BIT));
+                squareBits((long)(0*8+0), (byte) (ROOK_VALUE));
+                squareBits((long)(0*8+7), (byte) (ROOK_VALUE));
+                squareBits((long)(7*8+0), (byte) (ROOK_VALUE | WHITE_BIT ));
+                squareBits((long)(7*8+7), (byte) (ROOK_VALUE | WHITE_BIT ));
 
-                squareBits((long)(0*8+1), (byte) (KNIGHT_VALUE | BLACK_BIT | HOME_BIT));
-                squareBits((long)(0*8+6), (byte) (KNIGHT_VALUE | BLACK_BIT | HOME_BIT));
-                squareBits((long)(7*8+1), (byte) (KNIGHT_VALUE | WHITE_BIT | HOME_BIT));
-                squareBits((long)(7*8+6), (byte) (KNIGHT_VALUE | WHITE_BIT | HOME_BIT));
+                squareBits((long)(0*8+1), (byte) (KNIGHT_VALUE ));
+                squareBits((long)(0*8+6), (byte) (KNIGHT_VALUE ));
+                squareBits((long)(7*8+1), (byte) (KNIGHT_VALUE | WHITE_BIT ));
+                squareBits((long)(7*8+6), (byte) (KNIGHT_VALUE | WHITE_BIT ));
 
-                squareBits((long)(0*8+2), (byte) (BISHOP_VALUE | BLACK_BIT | HOME_BIT));
-                squareBits((long)(0*8+5), (byte) (BISHOP_VALUE | BLACK_BIT | HOME_BIT));
-                squareBits((long)(7*8+2), (byte) (BISHOP_VALUE | WHITE_BIT | HOME_BIT));
-                squareBits((long)(7*8+5), (byte) (BISHOP_VALUE | WHITE_BIT | HOME_BIT));
+                squareBits((long)(0*8+2), (byte) (BISHOP_VALUE ));
+                squareBits((long)(0*8+5), (byte) (BISHOP_VALUE ));
+                squareBits((long)(7*8+2), (byte) (BISHOP_VALUE | WHITE_BIT ));
+                squareBits((long)(7*8+5), (byte) (BISHOP_VALUE | WHITE_BIT ));
 
-                squareBits((long)(0*8+3), (byte) (QUEEN_VALUE | BLACK_BIT | HOME_BIT));
-                squareBits((long)(7*8+3), (byte) (QUEEN_VALUE | WHITE_BIT | HOME_BIT));
+                squareBits((long)(0*8+3), (byte) (QUEEN_VALUE ));
+                squareBits((long)(7*8+3), (byte) (QUEEN_VALUE | WHITE_BIT ));
 
-                squareBits((long)(0*8+4), (byte) (KING_VALUE | BLACK_BIT | HOME_BIT));
-                squareBits((long)(7*8+4), (byte) (KING_VALUE | WHITE_BIT | HOME_BIT));
+                squareBits((long)(0*8+4), (byte) (KING_VALUE ));
+                squareBits((long)(7*8+4), (byte) (KING_VALUE | WHITE_BIT ));
                 return this;
             }
 
@@ -275,47 +251,6 @@ public class Main {
             return schema.allocate(acc, length);
         }
     }
-
-    public static final byte DIAGS = (byte) 0b1010_0101;
-    public static final byte COLROWS = (byte) 0b0101_1010;
-    public static final byte DIAGS_OR_COLROWS = (byte) (DIAGS | COLROWS);
-
-    enum PIECE {
-        EMPTY(EMPTY_SQUARE, (byte) 0, false, 0),
-        PAWN(PAWN_VALUE, (byte) 0, false, 0),
-        KNIGHT(KNIGHT_VALUE, (byte) 0, true, 0),
-        BISHOP(BISHOP_VALUE, DIAGS, false, 7),
-        ROOK(ROOK_VALUE, COLROWS, true, 7),
-        QUEEN(QUEEN_VALUE, DIAGS_OR_COLROWS, false, 7),
-        KING(KING_VALUE, DIAGS_OR_COLROWS, false, 1),
-        OFFBOARD(OFF_BOARD_SQUARE, (byte) 0, false, 0);
-
-
-        public final int value;
-        public final byte compassBits;
-        public final boolean knight;
-
-        public final int count;
-
-        PIECE(int value, byte compassBits, boolean knight, int count) {
-            this.value = value;
-            this.compassBits = compassBits;
-            this.knight = knight;
-            this.count = count;
-        }
-
-        static public PIECE of(int squareBits) {
-            if (Compute.isKing(squareBits)) return KING;
-            else if (Compute.isQueen(squareBits)) return QUEEN;
-            else if (Compute.isRook(squareBits)) return ROOK;
-            else if (Compute.isBishop(squareBits)) return BISHOP;
-            else if (Compute.isKnight(squareBits)) return KNIGHT;
-            else if (Compute.isPawn(squareBits)) return PAWN;
-            else if (Compute.isOffBoard(squareBits)) return PIECE.OFFBOARD;
-            else return EMPTY;
-        }
-    }
-
 
     static class ChessDataImpl implements ChessData {
         static class BoardImpl implements ChessData.Board {
@@ -409,7 +344,7 @@ public class Main {
             int y = i / 8;
             byte squareBits = board.getSquareBits(x, y);
             if (!Compute.isEmpty(squareBits)) {
-                if (Compute.isMyPiece(WHITE_BIT, squareBits)) {
+                if (Compute.isWhite(squareBits)) {
                     board.validMoves(squareBits, x, y);
                 }
             }
